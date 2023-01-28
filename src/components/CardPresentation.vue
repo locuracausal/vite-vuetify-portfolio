4739cb027ed09540b4528d970de3aba42f60da27
icuadrado <icuadrado@xirectds.com>
2023-01-28 18:40:00 -0300

    Init portfolio

---
+++ src/components/CardPresentation.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card
+      class="mx-auto"
+      max-width="434"
+      rounded="20"
+    >
+      <v-img
+        height="100%"
+        cover
+        src="https://www.trio.dev/hubfs/Imported_Blog_Media/c346346f799a7980449c9ad34b7a634f-1-1.jpg"
+      >
+        <v-avatar
+          color="grey"
+          size="150"
+          rounded="0"
+        >
+          <v-img cover src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+        </v-avatar>
+        <v-list-item
+          class="text-white"
+          title="Ivan Cuadrado"
+          subtitle="Technical Software Developer"
+        ></v-list-item>
+      </v-img>
+    </v-card>
+  </template>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+      <v-row>
+        <v-col>
+          <Avatar />
+        </v-col>
+        <v-col>
+          <HelloWorld />  
+        </v-col>
+      </v-row>
+    </v-container>
+</template>
+
+<script lang="ts" setup>
+  import Avatar from '@/components/CardPresentation.vue'  
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
